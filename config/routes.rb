9a53b30d71c37d8fbca0d617b561db3b2156d14e
Waihon Yew <yewwaihon@gmail.com>
2018-09-17 18:43:23 +0800

    Add User and Shout page

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   end
 
   root to: "homes#show"
-  resources :shouts, only: [:create]
+  resources :shouts, only: [:create, :show]
 
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create] do
+  resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
