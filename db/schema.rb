a4793b31429956c1a51822ecb50b941f8cc214f2
Waihon Yew <yewwaihon@gmail.com>
2018-09-26 00:46:54 +0800

    Does not allow null for user_id, content_type, and content_id in shouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180917134945) do
+ActiveRecord::Schema.define(version: 20180925162612) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "shouts", force: :cascade do |t|
-    t.integer  "user_id"
+    t.integer  "user_id",      null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.string   "content_type"
-    t.integer  "content_id"
+    t.string   "content_type", null: false
+    t.integer  "content_id",   null: false
     t.index ["content_type", "content_id"], name: "index_shouts_on_content_type_and_content_id", using: :btree
     t.index ["user_id"], name: "index_shouts_on_user_id", using: :btree
   end
