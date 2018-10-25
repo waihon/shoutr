eb2282ae27e6f8a076284d05034c5d1f0c16341a
Waihon Yew <yewwaihon@gmail.com>
2018-10-25 23:58:35 +0800

    Update the shouts controller to be open for extension

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   end
 
   root to: "homes#show"
-  resources :shouts, only: [:create, :show] do
+
+  post "text_shouts" => "shouts#create", defaults: { content_type: TextShout }
+  post "photo_shouts" => "shouts#create", defaults: {content_type: PhotoShout }
+
+  resources :shouts, only: [:show] do
     member do
       post "like" => "likes#create"
       delete "unlike" => "likes#destroy"
