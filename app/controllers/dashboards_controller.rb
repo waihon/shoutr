910d88fe863a30940244ee2baab8e17a7b0f4b00
Waihon Yew <yewwaihon@gmail.com>
2018-10-28 10:46:57 +0800

    Extract Timeline and Dashboard classes
    Using Single Responsibility Principle

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,5 @@
 class DashboardsController < ApplicationController
   def show
-    @shout = Shout.new
-    @shouts = current_user.timeline_shouts
+    @dashboard = Dashboard.new(current_user)
   end
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -4,7 +4,5 @@ class Shout < ApplicationRecord
 
   validates :user, presence: true
 
-  default_scope { order(created_at: :desc) }
-
   delegate :username, to: :user
 end
