375edb60676edd14e9be7c5579559067046f9905
Waihon Yew <yewwaihon@gmail.com>
2018-10-28 16:44:08 +0800

    Simple search example

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -15,6 +15,9 @@ module ShoutHelper
   end
 
   def autolink(text)
-    text.gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1])}.html_safe
+    text.
+      gsub(/@\w+/) { |mention| link_to mention, user_path(mention[1..-1])}.
+      gsub(/#\w+/) { |hashtag| link_to hashtag, hashtag_path(hashtag[1..-1])}.
+      html_safe
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :hashtags, only: [:show]
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
