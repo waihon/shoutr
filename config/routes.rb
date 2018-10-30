a8253c32de1c466013d697945602401732f920de
Waihon Yew <yewwaihon@gmail.com>
2018-10-30 23:43:01 +0800

    Allow for searching on tedt body

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   root to: "homes#show"
 
+  resource :search, only: [:show]
+
   post "text_shouts" => "shouts#create", defaults: { content_type: TextShout }
   post "photo_shouts" => "shouts#create", defaults: {content_type: PhotoShout }
 
