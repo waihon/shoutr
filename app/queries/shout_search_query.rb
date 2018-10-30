2430197200164f70539dae5c9ff0f81fd440abe0
Waihon Yew <yewwaihon@gmail.com>
2018-10-31 00:26:10 +0800

    Allow for searching photo shouts

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -4,12 +4,27 @@ class ShoutSearchQuery
   end  
 
   def to_relation
-    Shout.
-      joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id").
-      where("text_shouts.body LIKE ?", "%#{term}%")
+    matching_shouts_for_text_shouts.
+      or(matching_shouts_for_photo_shouts)
   end
 
   private
 
   attr_reader :term
+
+  def matching_shouts_for_text_shouts
+    Shout.where(content_type: 'TextShout', content_id: matching_text_shouts.select(:id))
+  end
+
+  def matching_text_shouts
+    TextShout.where("body LIKE ?", "%#{term}%")
+  end
+
+  def matching_shouts_for_photo_shouts
+    Shout.where(content_type: 'PhotoShout', content_id: matching_photo_shouts.select(:id))
+  end
+
+  def matching_photo_shouts
+    PhotoShout.where("photo_shouts.image_file_name LIKE ?", "%#{term}%")
+  end
 end
